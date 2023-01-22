299c09faa488124aed9f0a652fc7cd04d42dc17a
TEJALMEHETRE <123307083+TEJALMEHETRE@users.noreply.github.com>
2023-01-22 19:24:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("my parents new healthy dinnar")
+streamlit.header("breakfast menu")
+streamlit.text("omega 3 and Blueberry Oatmeal")
+streamlit.text(" kale spinach and rocket smoothie")
+streamlit.text("hard boiled free range Egg")
+
