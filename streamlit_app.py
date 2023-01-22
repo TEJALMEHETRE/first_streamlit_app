ee11cc22ead8485a8770e94064e2e59b025e85f3
TEJALMEHETRE <123307083+TEJALMEHETRE@users.noreply.github.com>
2023-01-22 19:29:14 +0530

    Update streamlit_app.py
    added emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title("my parents new healthy dinnar")
 streamlit.header("breakfast menu")
-streamlit.text("omega 3 and Blueberry Oatmeal")
-streamlit.text(" kale spinach and rocket smoothie")
-streamlit.text("hard boiled free range Egg")
-
+streamlit.text(" 🥣 omega 3 and Blueberry Oatmeal")
+streamlit.text(" 🥬 kale spinach and rocket smoothie")
+streamlit.text(" 🥚 hard boiled free range Egg")
